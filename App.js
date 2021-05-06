7ebe8e318578c86d77d75573ac129ad2591fc0a6
Tayse Rosa <tayse_rosa@yahoo.com.br>
2021-05-06 05:51:17 -0300

    Features Slider React Native

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Slider from '@react-native-community/slider';
+
 export default function App() {
+  const [valor, setValor] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize:25}}> React Native - Slider  </Text>
+
+      <Slider 
+        style={{width:300, height:50}}
+        minimumValue={0}
+        maximumValue={100}
+        onValueChange={ (valSelecionado) => setValor(valSelecionado) }
+        value={valor}
+      />
+      
+      <Text style={{color: 'blue', fontSize:20}}> O valor atual é: {valor} </Text>
+
     </View>
   );
 }
